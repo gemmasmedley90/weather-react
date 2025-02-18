093a95905091addd170cb67ec176b4f0ce7cb6d0
gemmasmedley90 <gemmasmedley90@gmail.com>
2025-02-18 14:53:20 +0000

    Added axios to show weather of a city

---
+++ src/Weather.js
@@ -0,0 +1,24 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+  let apiKey = "c6f8ef4575250284954db9f4dfa7a996";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Audio
+      height="80"
+      width="80"
+      radius="9"
+      color="green"
+      ariaLabel="loading"
+      wrapperStyle
+      wrapperClass
+    />
+  );
+}
